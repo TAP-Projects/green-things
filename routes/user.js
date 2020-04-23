178be0651840035225dfea5d0f3b8a2b323a9f5d
github username <github email address>
2020-04-23 13:02:09 -0400

    sign up route

--- routes/user.js
+++ routes/user.js
@@ -1,23 +1,52 @@
-var express = require('express');
-var router = express.Router();
-const db = require('../db');
+/** @format */
 
-/* GET users listing. */
-//!NOTE: Not sure about the parameter here :id
-router.get('/:username', getUserByUsername);
+var express = require("express");
+var router = express.Router();
+const db = require("../db");
+
+/* GET sign up form */
+router.get("/", (req, res, next) => {
+	res.render("sign-up");
+});
+
+/* GET user profile. */
+router.get("/:username", getUserByUsername);
 
 function getUserByUsername(req, res, next) {
-    const username = req.params.username;
-    
-    //! NOTE: You can do this with async/await rather than a callback
-    //! NOTE: You can also use a config object in pool.query() rather than passing in the query, any parameters, and the callback. The parameters array is omitted here, because there aren't any.
+	const username = req.params.username;
 
-    // $1 represents the id we supply in the 2nd parameter
-    db.query('SELECT * FROM green_user WHERE username = $1', [username], (error, results) => {
-        if (error) { return next(error) }
-        console.log(results.rows[0]);
-        res.status(200).render('profile-page', results.rows[0]);
-    });
-};
+	// $1 represents the id we supply in the 2nd parameter
+	db.query(
+		"SELECT * FROM green_user WHERE username = $1;",
+		[username],
+		(error, results) => {
+			if (error) {
+				return next(error);
+			}
+			console.log(results.rows[0]);
+			res.status(200).render("profile-page", results.rows[0]);
+		}
+	);
+}
+
+/* POST sign up form */
+router.post("/", (req, res, next) => {
+	try {
+		const { firstName, lastName, email, username, password } = req.body;
+
+		db.query(
+			"INSERT INTO public.green_user(first_name,last_name,email,username,passwd) VALUES($1,$2,$3,$4,$5);",
+			[firstName, lastName, email, username, password],
+			(error, results) => {
+				if (error) {
+					return next(error);
+				}
+				res.redirect(`/user/:${username}`);
+			}
+		);
+	} catch (err) {
+		console.error(err);
+	}
+});
 
 module.exports = router;
