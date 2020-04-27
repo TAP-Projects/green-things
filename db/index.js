e269d947a184aecbcaeec9910d570da1ce99ed3e
github username <github email address>
2020-04-26 20:46:15 -0400

    db index edits for heroku

--- db/index.js
+++ db/index.js
@@ -1,17 +1,29 @@
-const config = require('dotenv').config();
-const express = require('express');
-const Pool = require('pg').Pool;
-const pool = new Pool();
+/** @format */
+
+const config = require("dotenv").config();
+const express = require("express");
+const Pool = require("pg").Pool;
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const connectionString = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`;
+
+const pool = new Pool({
+	connectionString: isProduction
+		? process.env.DATABASE_URL
+		: connectionString,
+	ssl: isProduction,
+});
 
 module.exports = {
-    query: (text, params) => pool.query(text, params),
-    testConnection: async () => {
-        try {
-            const connected = await pool.connect();
-            console.log("Connected to ", connected.database);
-            //await pool.end();
-        } catch (err) {
-            console.error(err);
-        }
-    }
+	query: (text, params) => pool.query(text, params),
+	testConnection: async () => {
+		try {
+			const connected = await pool.connect();
+			console.log("Connected to ", connected.database);
+			//await pool.end();
+		} catch (err) {
+			console.error(err);
+		}
+	},
 };
